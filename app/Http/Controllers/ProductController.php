f822152c6a6cdf2eba5553d62ff26885b3caa66c
anhnhph33991 <hoang838604@gmail.com>
2024-07-15 19:03:51 +0700

    laravel-kid

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+//        Yêu cầu 1: Truy vấn tất cả các bản ghi:
+
+    }
+}
